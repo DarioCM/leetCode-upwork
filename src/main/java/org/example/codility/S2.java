54f9850b500b8fa982cf08ff92105eb5f3110691
Carlos D <dario20049@icloud.com>
2024-07-19 15:48:24 +0900

    MergeTwoSortedLists.java

--- src/main/java/org/example/codility/S2.java
+++ src/main/java/org/example/codility/S2.java
@@ -5,40 +5,27 @@ import java.util.HashMap;
 public class S2 {
 
     public static void main(String[] args) {
-        System.out.println(solution("Wed", 2));
-        System.out.println(solution("Sat", 23));
+
     }
 
-    // Days of the week are represented as three-letter strings ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun").
-    // Write a function solution that, given a string S representing the day of the week and an integer K (0 ≤ K ≤ 500), returns the day of the week that is K days later.
-    // For example, given S = "Wed" and K = 2, the function should return "Fri".
-    //  Given S = "Sat" and K = 23, the function should return "Mon".
-    public static String solution(String s, int k) {
 
-        // change this from an array to a map
-       /* HashMap<String,Integer> map = new HashMap<>();
-        days.put("Mon", 0);
-        days.put("Tue", 1);
-        days.put("Wed", 2);
-        days.put("Thu", 3);
-        days.put("Fri", 4);
-        days.put("Sat", 5);
-        days.put("Sun", 6);
-*/
-        String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    // return string true if num is a prime number, otherwise return false
+    public static String solution(int num) {
 
-        int index = 0;
-
-        for (String day : days) {
-            if (day.equals(s)) {
-                break;
-            }
-            index++;
+        if (num < 2) {
+            return "false";
         }
 
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return "false";
+            }
+        }
+
+        return "true";
 
 
-        return days[(index + k) % 7];
+
     }
 
 }
--- src/main/java/org/example/easy/MergeTwoSortedLists.java
+++ src/main/java/org/example/easy/MergeTwoSortedLists.java
@@ -4,11 +4,34 @@ public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        ListNode result = new ListNode(0);
-        ListNode current = result;
+        ListNode preHead = new ListNode(-1);
+        ListNode prev = preHead;
 
+        while (list1 != null  && list2 != null){
+            if (list1.val <= list2.val ) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+        }
 
-        return null;
+        prev.next = (list1 == null) ? list2 : list1;
+
+        return preHead.next;
+    }
+
+    public static void main(String[] args) {
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        ListNode list1 = mergeTwoSortedLists.new ListNode(1, mergeTwoSortedLists.new ListNode(2, mergeTwoSortedLists.new ListNode(4)));
+        ListNode list2 = mergeTwoSortedLists.new ListNode(1, mergeTwoSortedLists.new ListNode(3, mergeTwoSortedLists.new ListNode(4)));
+        ListNode result = mergeTwoSortedLists.mergeTwoLists(list1, list2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
     }
 
     /**
